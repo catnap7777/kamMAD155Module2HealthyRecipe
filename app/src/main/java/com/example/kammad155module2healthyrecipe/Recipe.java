037ea4bdf240cb18a29cde8503b2d6a453a0a607
km <35552602+catnap7777@users.noreply.github.com>
2022-05-18 13:03:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/kammad155module2healthyrecipe/Recipe.java
@@ -0,0 +1,13 @@
+package com.example.kammad155module2healthyrecipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}
